02d1a046f7671b0118595230b578b597ae27b6be
Kalvinci <kalyanasundharam97@gmail.com>
2021-09-28 01:04:42 -0400

    multiple fields support enabled for heatmap

--- heatmap.py
+++ heatmap.py
@@ -5,46 +5,45 @@ import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-def generate_linklab_heatmap(start_datetime, end_datetime, field, export_filepath):
-	try:
-		df = pd.read_csv("book_with_grids.csv")
-		grid_map = {}
-		for row in df.itertuples():
-			if row.grid in grid_map:
-				grid_map[row.grid].append(row.device_id)
-			else:
-				grid_map[row.grid] = [row.device_id]
+def generate_linklab_heatmap(start_datetime, end_datetime, fields, export_filepath):
+	df = pd.read_csv("book_with_grids.csv")
+	grid_deviceids_map = {}
+	for row in df.itertuples():
+		if row.grid in grid_deviceids_map:
+			grid_deviceids_map[row.grid].append(row.device_id)
+		else:
+			grid_deviceids_map[row.grid] = [row.device_id]
 
-		deviceid_datapoint_countmap = util.get_deviceid_datapoint_countmap(field, start_datetime, end_datetime, list(df["device_id"]))
+	deviceid_datapoint_countmap = util.get_deviceid_datapoint_countmap(fields, start_datetime, end_datetime, list(df["device_id"]))
 
-		data = []
-		row = 0
-		column = 'A'
-		for grid in range(200):
-			data_points = 0
-			if grid in grid_map:
-				for device_id in grid_map[grid]:
-					if device_id in deviceid_datapoint_countmap:
-						data_points += deviceid_datapoint_countmap[device_id]
-			data.append([column, row, data_points])
-			column = chr(ord(column) + 1)
-			if column == "U":
-				row += 1
-				column = "A"
-				
-		grid_df = pd.DataFrame(data, columns=["Column", "Row", "data_points"])
-		grid_df = grid_df.pivot(index="Row", columns="Column", values="data_points")
-		
-		plt.figure(figsize=(20, 8))
-		ax = sns.heatmap(grid_df, linewidths=1, cmap="YlGnBu", annot=True, fmt="d")
-		plt.title(f"LinkLab Heatmap - {field}")
-		filepath = f"{export_filepath}/LinkLab_Heatmap-{field}.png"
-		plt.savefig(filepath)
-		
-		return filepath
-	except:
-		return None
+	data = []
+	row = 0
+	column = 'A'
+	for grid in range(200):
+		data_points = 0
+		if grid in grid_deviceids_map:
+			for device_id in grid_deviceids_map[grid]:
+				if device_id in deviceid_datapoint_countmap:
+					data_points += deviceid_datapoint_countmap[device_id]
+		data.append([column, row, grid, data_points])
+		column = chr(ord(column) + 1)
+		if column == "U":
+			row += 1
+			column = "A"
+			
+	grid_df = pd.DataFrame(data, columns=["Column", "Row", "grid", "data_points"])
+	# grid_df.to_csv("grid_datapoints.csv", index=False)
+	grid_df = grid_df.pivot(index="Row", columns="Column", values="data_points")
+	
+	plt.figure(figsize=(25, 10))
+	ax = sns.heatmap(grid_df, linewidths=1, cmap="YlGnBu", annot=True, fmt="d")
+	field_str = ",".join(fields)
+	plt.title(f"LinkLab Heatmap - [{field_str}]")
+	filepath = f"{export_filepath}/LinkLab_Heatmap.png"
+	plt.savefig(filepath)
+
+	return filepath
 
 start_datetime = datetime(2021,1,1)
 end_datetime = datetime(2021,9,23)
-generate_linklab_heatmap(start_datetime, end_datetime, field="Illumination_lx", export_filepath="./img")
+generate_linklab_heatmap(start_datetime, end_datetime, fields=["Illumination_lx","Range select","Supply voltage_V","rssi"], export_filepath="./img")
